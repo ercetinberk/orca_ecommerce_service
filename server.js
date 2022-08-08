032b4187a26683805d42a8b39021b06dd606e099
berk ercetin <berke@orcabs.com>
2022-08-08 13:04:39 +0300

    server.js  changed

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ dotenv.config({ path: "./config/env/config.env" }); // bunun yerini degistirme!!
 const app = require("./app");
 const port = process.env.PORT;
 //192.168.1.39
-app.listen(port, "localhost", () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`App running on port ${port}...`);
 });
